43d10c2129b4da1a64078d058e7815df19a8b844
joe-shajan <joeshajan1551@gmail.com>
2023-05-22 19:55:19 +0530

    fix: removed unused files

--- client/src/routes/protected.tsx
+++ client/src/routes/protected.tsx
@@ -5,10 +5,6 @@ import { Spinner } from '@/components/Elements';
 import { MainLayout, SettingsLayout } from '@/components/Layout';
 import { lazyImport } from '@/utils/lazyImport';
 
-const { DiscussionsRoutes } = lazyImport(
-  () => import('@/features/discussions'),
-  'DiscussionsRoutes'
-);
 const { Dashboard } = lazyImport(() => import('@/features/misc'), 'Dashboard');
 // const { Profile } = lazyImport(() => import('@/features/users'), 'Profile');
 const { Users } = lazyImport(() => import('@/features/users'), 'Users');
@@ -122,7 +118,6 @@ export const protectedRoutes = [
     path: '/app',
     element: <App />,
     children: [
-      { path: '/discussions/*', element: <DiscussionsRoutes /> },
       { path: '/users', element: <Users /> },
       // { path: '/profile', element: <Profile /> },
       { path: '/', element: <Dashboard /> },
