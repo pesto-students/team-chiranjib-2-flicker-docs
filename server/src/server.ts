793959b23cc0c38f05480b83564d4948cab949e2
joe-shajan <joeshajan1551@gmail.com>
2023-06-01 21:27:11 +0530

    create doc based on user

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
-import { TestRoute } from '@routes/test.route';
+import { DocumentRoute } from '@routes/document.route';
 import { ValidateEnv } from '@utils/validateEnv';
 
 ValidateEnv();
 
-const app = new App([new UserRoute(), new AuthRoute(), new TestRoute()]);
+const app = new App([new UserRoute(), new AuthRoute(), new DocumentRoute()]);
 
 app.listen();
