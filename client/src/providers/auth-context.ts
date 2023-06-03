87e561737d82ba6dc9b1de9f7be14cfb4f0b9198
joe-shajan <joeshajan1551@gmail.com>
2023-06-03 16:00:19 +0530

    auth context update

--- client/src/providers/auth-context.ts
+++ client/src/providers/auth-context.ts
@@ -1,13 +1,22 @@
 import React from 'react';
 
 // Creating the context object and passing the default values.
-const user = {
-  email: '',
-  firstName: '',
-  lastName: '',
-  picture: '',
-};
 
-const authContext = React.createContext({ user });
+export type User = {
+  _id: string;
+  email: string;
+  name: string;
+  firstName: string;
+  lastName: string;
+  picture: string;
+} | null;
+
+const user: User = null;
+
+interface AuthContextType {
+  user: User;
+}
+
+const authContext = React.createContext<AuthContextType>({ user });
 
 export default authContext;
