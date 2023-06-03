1fb4cadedb5e62f6474d9571720959bd927dd21d
joe-shajan <joeshajan1551@gmail.com>
2023-06-04 01:33:30 +0530

    seperate document controllers and services

--- server/src/controllers/document.controller.ts
+++ server/src/controllers/document.controller.ts
@@ -1,10 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
-import mongoose from 'mongoose';
 
-import { HttpException } from '@/exceptions/httpException';
-import { DocumentModel } from '@/models/document.model';
-import { UserModel } from '@/models/users.model';
 import { DocumentService } from '@/services/document.service';
 // import { User } from '@interfaces/users.interface';
 // import { TestService } from '@services/test.service';
@@ -17,44 +13,23 @@ export class DocumentController {
 
     try {
       await this.controller.createDoc(docName, user);
-      res.send();
+      res.send('document created successfully');
     } catch (error) {
       next(error);
     }
   };
 
-  public getDocs = async (req: Request, res: Response) => {
+  public getDocs = async (req: Request, res: Response, next: NextFunction) => {
     const { user, docType } = req.query as { user: any; docType: any };
 
-    const getQuery = (fieldName: string | string[]) => {
-      return UserModel.findOne({ email: user?.email }).populate(fieldName);
-    };
-
-    let query: any;
-
-    if (+docType === 0) {
-      query = getQuery(['documents', 'sharedDocuments']);
-    } else if (+docType === 1) {
-      query = getQuery('documents');
-    } else if (+docType === 2) {
-      query = getQuery('sharedDocuments');
-    }
-
-    if (query) {
-      query.exec((err, user) => {
-        if (err) return;
-        if (!user) return;
-
-        let documents: any;
-        if (+docType === 0) {
-          documents = [...user.documents, ...user.sharedDocuments];
-        } else if (+docType === 1) {
-          documents = user.documents;
-        } else if (+docType === 2) {
-          documents = user.sharedDocuments;
-        }
+    try {
+      const callbackfn = documents => {
         return res.json({ documents });
-      });
+      };
+
+      this.controller.getDocs(user, docType, callbackfn);
+    } catch (error) {
+      next(error);
     }
   };
 
@@ -62,21 +37,7 @@ export class DocumentController {
     const { userId, documentName } = req.body;
 
     try {
-      const user = await UserModel.findById(userId);
-      if (!user) throw new HttpException(404, 'User not found');
-
-      const document = await DocumentModel.findOne({ name: documentName });
-      if (!document) throw new HttpException(404, 'Document not found');
-
-      const documentId = user.documents.find(doc => new mongoose.Types.ObjectId(doc).equals(document._id));
-      if (documentId) throw new HttpException(400, 'You are the owner of the document');
-
-      const sharedDocumentId = user.sharedDocuments.find(doc => new mongoose.Types.ObjectId(doc).equals(document._id));
-      if (sharedDocumentId) throw new HttpException(400, 'Document already shared');
-
-      user.sharedDocuments.push(document._id);
-      user.save();
-
+      await this.controller.addSharedDocumentToUser(userId, documentName);
       return res.send('success');
     } catch (error) {
       next(error);
--- server/src/services/document.service.ts
+++ server/src/services/document.service.ts
@@ -1,30 +1,74 @@
+import { HttpException } from '@/exceptions/httpException';
 import { User } from '@/interfaces/users.interface';
 import { DocumentModel } from '@/models/document.model';
 import { UserModel } from '@/models/users.model';
+import mongoose from 'mongoose';
 import { Service } from 'typedi';
 
 @Service()
 export class DocumentService {
   public createDoc = async (docName: string, user: User) => {
-    try {
-      const createDocData = new DocumentModel({
-        name: docName,
-        displayName: 'new doc',
-        createdAt: new Date(),
+    const createDocData = new DocumentModel({
+      name: docName,
+      displayName: 'new doc',
+      createdAt: new Date(),
+    });
+
+    const document = await createDocData.save();
+
+    const userData = await UserModel.findOne({ email: user.email });
+
+    userData.documents.push(document._id);
+    userData.save();
+  };
+
+  public getDocs = (user: any, docType: string, cb: any) => {
+    const getQuery = (fieldName: string | string[]) => {
+      return UserModel.findOne({ email: user?.email }).populate(fieldName);
+    };
+
+    let query: any;
+
+    if (+docType === 0) {
+      query = getQuery(['documents', 'sharedDocuments']);
+    } else if (+docType === 1) {
+      query = getQuery('documents');
+    } else if (+docType === 2) {
+      query = getQuery('sharedDocuments');
+    }
+
+    if (query) {
+      let documents: any;
+      query.exec((err, user) => {
+        if (err) return;
+        if (!user) return;
+
+        if (+docType === 0) {
+          documents = [...user.documents, ...user.sharedDocuments];
+        } else if (+docType === 1) {
+          documents = user.documents;
+        } else if (+docType === 2) {
+          documents = user.sharedDocuments;
+        }
+        cb(documents);
       });
-
-      const document = await createDocData.save();
-
-      const userData = await UserModel.findOne({ email: user.email });
-
-      userData.documents.push(document._id);
-      userData.save();
-    } catch (error) {
-      console.log(error);
     }
   };
 
-  //   public getDocs = async () => {};
+  public addSharedDocumentToUser = async (userId, documentName) => {
+    const user = await UserModel.findById(userId);
+    if (!user) throw new HttpException(404, 'User not found');
 
-  //   public addSharedDocumentToUser = async () => {};
+    const document = await DocumentModel.findOne({ name: documentName });
+    if (!document) throw new HttpException(404, 'Document not found');
+
+    const documentId = user.documents.find(doc => new mongoose.Types.ObjectId(doc).equals(document._id));
+    if (documentId) throw new HttpException(400, 'You are the owner of the document');
+
+    const sharedDocumentId = user.sharedDocuments.find(doc => new mongoose.Types.ObjectId(doc).equals(document._id));
+    if (sharedDocumentId) throw new HttpException(400, 'Document already shared');
+
+    user.sharedDocuments.push(document._id);
+    user.save();
+  };
 }
