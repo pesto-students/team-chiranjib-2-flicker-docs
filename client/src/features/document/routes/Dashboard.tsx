9cb0d623df771114c4dd06590a32bff48dd7e818
joe-shajan <joeshajan1551@gmail.com>
2023-06-03 13:09:47 +0530

    create document mutation

--- client/src/features/document/routes/Dashboard.tsx
+++ client/src/features/document/routes/Dashboard.tsx
@@ -1,23 +1,31 @@
-import axios from 'axios';
+import { useMutation } from '@tanstack/react-query';
 import { Plus } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import uniqid from 'uniqid';
 
 import { Button, Header } from '@/components';
-import { API_URL } from '@/config';
 import { useAuth } from '@/hooks';
+import { axiosClient } from '@/lib';
 
 import DocumentTabs from '../components/DocumentTabs';
 
 export const Dashboard = () => {
   const navigate = useNavigate();
   const { user } = useAuth();
+
+  const mutation = useMutation({
+    mutationFn: (docName: string) => {
+      return axiosClient.post(`/document`, {
+        user,
+        docName,
+      });
+    },
+  });
+
   const createANewDocument = async () => {
     const docName = uniqid();
-    await axios.post(`${API_URL}/document`, {
-      user,
-      docName,
-    });
+
+    mutation.mutate(docName);
 
     navigate(`/editor/${docName}`);
   };
