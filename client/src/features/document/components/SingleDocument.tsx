288c18f31dd21372468929dc475a7407265318f4
joe-shajan <joeshajan1551@gmail.com>
2023-06-06 16:36:52 +0530

    shadow

--- client/src/features/document/components/SingleDocument.tsx
+++ client/src/features/document/components/SingleDocument.tsx
@@ -12,25 +12,25 @@ type SingleDocumentProps = {
 
 const SingleDocument = ({ displayName, createdAt, updatedAt, name }: SingleDocumentProps) => {
   return (
-    <Link to={`/editor/${name}`}>
-      <div className='h-64 overflow-hidden rounded-md border border-slate-200 shadow-sm'>
+    <div className='h-64 overflow-hidden rounded-md border border-slate-200 shadow-sm hover:shadow-md'>
+      <Link to={`/editor/${name}`}>
         <div className='h-[70%]'></div>
-        <div className='flex h-[30%] gap-2 border-t p-4'>
-          <div className='flex h-full w-[90%] flex-col justify-between'>
-            <p className=' truncate text-sm text-slate-600'>{displayName}</p>
-            <div className='flex'>
-              <FileText className='h-4' />
-              <p className='truncate text-xs text-slate-600'>
-                Opened {dayjs(updatedAt ? updatedAt : createdAt).format('MMMM D, YYYY')}
-              </p>
-            </div>
-          </div>
-          <div className='flex h-full items-center'>
-            <MoreVertical />
+      </Link>
+      <div className='flex h-[30%] gap-2 border-t p-4'>
+        <div className='flex h-full w-[90%] flex-col justify-between'>
+          <p className=' truncate text-sm text-slate-600'>{displayName}</p>
+          <div className='flex'>
+            <FileText className='h-4' />
+            <p className='truncate text-xs text-slate-600'>
+              Opened {dayjs(updatedAt ? updatedAt : createdAt).format('MMMM D, YYYY')}
+            </p>
           </div>
         </div>
+        <div className='flex h-full items-center'>
+          <MoreVertical />
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
