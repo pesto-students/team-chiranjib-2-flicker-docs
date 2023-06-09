e71059f3a0421e0ad95c9c007de0a400eb9cee52
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 01:42:38 +0530

    reexport stripe secret

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,4 +3,4 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
-export const { DB_HOST, DB_PORT, DB_DATABASE, DB_URL, GOOGLE_OAUTH_CLIENT_ID } = process.env;
+export const { DB_HOST, DB_PORT, DB_DATABASE, DB_URL, GOOGLE_OAUTH_CLIENT_ID, STRIPE_SECRET_KEY } = process.env;
