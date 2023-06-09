e92fecdbc3bd27785c6bb0008c57a9d47b5dcc4f
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 03:45:33 +0530

    fix: drop down link

--- client/src/components/AvatarWithDropdown.tsx
+++ client/src/components/AvatarWithDropdown.tsx
@@ -1,6 +1,8 @@
 import { CreditCard, LogOut, Settings, User } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from '@/hooks';
+import { logout } from '@/lib';
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import {
@@ -15,7 +17,7 @@ import {
 
 export function AvatarWithDropdown() {
   const { user } = useAuth();
-
+  const navigate = useNavigate();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -31,21 +33,21 @@ export function AvatarWithDropdown() {
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate('/settings/profile')}>
             <User className='mr-2 h-4 w-4' />
             <span>Profile</span>
           </DropdownMenuItem>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate('/settings/my-plan')}>
             <CreditCard className='mr-2 h-4 w-4' />
             <span>Yourplan</span>
           </DropdownMenuItem>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate('/plans')}>
             <Settings className='mr-2 h-4 w-4' />
             <span>Subscription</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={logout}>
           <LogOut className='mr-2 h-4 w-4' />
           <span>Log out</span>
         </DropdownMenuItem>
