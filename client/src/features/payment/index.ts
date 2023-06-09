e77aee0bd3fc2d3c3d5224cc67d9376a873aa8bc
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 03:46:02 +0530

    fix: removed unused code

--- client/src/features/payment/index.ts
+++ client/src/features/payment/index.ts
@@ -1,3 +1,2 @@
 export * from './routes/Plans';
-export * from './routes/Success';
 export * from './routes/Cancel';
--- client/src/routes/protected.tsx
+++ client/src/routes/protected.tsx
@@ -1,14 +1,8 @@
-// import { Suspense } from 'react';
-// import { Outlet } from 'react-router-dom';
-
-// import { SettingsLayout } from '@/components/Layout/ProfileLayout';
 import { lazyImport } from '@/utils/lazyImport';
 const { Editor } = lazyImport(() => import('@/features/document'), 'Editor');
 const { Dashboard } = lazyImport(() => import('@/features/document'), 'Dashboard');
 const { Plans } = lazyImport(() => import('@/features/payment'), 'Plans');
-const { Success } = lazyImport(() => import('@/features/payment'), 'Success');
 const { Cancel } = lazyImport(() => import('@/features/payment'), 'Cancel');
-
 const { Settings } = lazyImport(() => import('@/features/profile'), 'Settings');
 const { Profile } = lazyImport(() => import('@/features/profile'), 'Profile');
 const { MyPlan } = lazyImport(() => import('@/features/profile'), 'MyPlan');
@@ -25,10 +19,6 @@ export const protectedRoutes = [
   {
     path: '/plans',
     element: <Plans />,
-  },
-  {
-    path: '/plans/success',
-    element: <Success />,
   },
   {
     path: '/plans/cancel',
