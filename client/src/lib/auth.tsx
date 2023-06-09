6f3970d578f5579dab4e405d17e6d369552f6339
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 03:46:24 +0530

    redirect url after logout

--- client/src/lib/auth.tsx
+++ client/src/lib/auth.tsx
@@ -3,7 +3,9 @@ import storage from '@/utils/storage';
 
 export const logout = () => {
   storage.clearToken();
-  window.location.reload();
+  window.location.replace(
+    process.env.REACT_APP_URL ? process.env.REACT_APP_URL : 'http://localhost:3000'
+  );
 };
 
 export const login = (token: any) => {
