0b817cb4f55f49bcec7df88efb6dec3f02a5e170
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 02:04:37 +0530

    cancel redirect page

--- client/src/features/payment/routes/Cancel.tsx
+++ client/src/features/payment/routes/Cancel.tsx
@@ -1,3 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components';
+
 export const Cancel = () => {
-  return <div>Cancel</div>;
+  const navigate = useNavigate();
+  return (
+    <div className='mt-10 flex flex-col items-center gap-6'>
+      <p className='text-xl text-red-400'>Oops! It seems like there was a payment error. 🥹</p>
+      <Button onClick={() => navigate('/plans')}>Go back</Button>
+    </div>
+  );
 };
