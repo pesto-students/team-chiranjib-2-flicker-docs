2cb0aa19392df0bfa89bf19a4881e4dcda47a8bf
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 12:08:27 +0530

    update doc controller

--- server/src/controllers/document.controller.ts
+++ server/src/controllers/document.controller.ts
@@ -2,8 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
 
 import { DocumentService } from '@/services/document.service';
-import { DocumentModel } from '@/models/document.model';
-import { UserModel } from '@/models/users.model';
 // import { User } from '@interfaces/users.interface';
 // import { TestService } from '@services/test.service';
 
@@ -14,22 +12,6 @@ export class DocumentController {
     const { docName, user } = req.body;
 
     try {
-      const createDocData = new DocumentModel({
-        name: docName,
-        displayName: 'new doc',
-        createdAt: new Date(),
-      });
-
-      const document = await createDocData.save();
-
-      const userData = await UserModel.findOne({ email: user.email });
-
-      if (userData?.documents?.length > 0) {
-        userData.documents.push(document._id);
-        userData.save();
-      }
-
-      res.send();
       await this.controller.createDoc(docName, user);
       res.send('document created successfully');
     } catch (error) {
