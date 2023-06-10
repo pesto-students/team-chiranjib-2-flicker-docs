d76f73ba9cca368614cac5f8a9354f8e1989036f
joe-shajan <joeshajan1551@gmail.com>
2023-06-10 10:22:08 +0530

    fix: selected plan highlight

--- client/src/features/payment/components/PlanCards.tsx
+++ client/src/features/payment/components/PlanCards.tsx
@@ -14,28 +14,36 @@ type PlanProps = {
 };
 
 const SinglePlan = ({ plan, handleCheckout, selected }: PlanProps) => (
-  <div className='mx-auto flex w-full flex-col rounded-lg border border-gray-100 bg-white p-6 text-center text-gray-900 shadow hover:shadow-lg dark:border-gray-600 dark:bg-gray-800 dark:text-white xl:p-8'>
-    <h3 className='mb-4 text-2xl font-semibold'>{plan.heading}</h3>
-    <p className='font-light text-gray-500 dark:text-gray-400 sm:text-lg'>{plan.title}</p>
-    <div className='my-8 flex items-baseline justify-center'>
-      <span className='mr-2 text-5xl font-extrabold'>${plan.price}</span>
-      <span className='text-gray-500 dark:text-gray-400'>/month</span>
+  <div
+    className={`mx-auto flex w-full flex-col rounded-lg ${
+      selected
+        ? 'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-0.5'
+        : 'bg-gray-100 p-[1px]'
+    }   text-center text-gray-900 shadow hover:shadow-lg`}
+  >
+    <div className='flex h-full flex-col rounded-lg bg-white p-6'>
+      <h3 className='mb-4 text-2xl font-semibold'>{plan.heading}</h3>
+      <p className='font-light text-gray-500 dark:text-gray-400 sm:text-lg'>{plan.title}</p>
+      <div className='my-8 flex items-baseline justify-center'>
+        <span className='mr-2 text-5xl font-extrabold'>${plan.price}</span>
+        <span className='text-gray-500 dark:text-gray-400'>/month</span>
+      </div>
+
+      <ul className='mb-8 space-y-4 text-left'>
+        {plan.features.map((feature) => (
+          <li key={feature.title} className='flex items-center space-x-3'>
+            {feature.icon}
+            <span>{feature.title}</span>
+          </li>
+        ))}
+      </ul>
+
+      {plan.heading !== 'Starter' && (
+        <Button onClick={() => handleCheckout(plan.heading)}>
+          {selected ? 'Your current plan' : 'Choose Plan'}
+        </Button>
+      )}
     </div>
-
-    <ul className='mb-8 space-y-4 text-left'>
-      {plan.features.map((feature) => (
-        <li key={feature.title} className='flex items-center space-x-3'>
-          {feature.icon}
-          <span>{feature.title}</span>
-        </li>
-      ))}
-    </ul>
-
-    {plan.heading !== 'Starter' && (
-      <Button onClick={() => handleCheckout(plan.heading)}>
-        {selected ? 'Your current plan' : 'Choose Plan'}
-      </Button>
-    )}
   </div>
 );
 
