e71466932cd069eb1f5d4bd479a4ff3d9ce66755
joe-shajan <joeshajan1551@gmail.com>
2023-06-12 05:59:59 +0530

    progress bar

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,6 +4,7 @@ export * from './ui/avatar';
 export * from './ui/skeleton';
 export * from './ui/tabs';
 export * from './ui/label';
+export * from './ui/progress';
 
 export * from './AvatarWithDropdown';
 export * from './Header';
