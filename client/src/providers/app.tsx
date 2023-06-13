34d27fdaf79b36740df4655e93b950ce9eb3124b
joe-shajan <joeshajan1551@gmail.com>
2023-06-13 22:29:29 +0530

    fix: update error fallback

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -18,9 +18,11 @@ const ErrorFallback = () => {
       role='alert'
     >
       <h2 className='text-lg font-semibold'>Ooops, something went wrong :( </h2>
-      <Button className='mt-4' onClick={() => window.location.assign(window.location.origin)}>
-        Refresh
-      </Button>
+
+      <div className='mt-4 flex gap-4'>
+        <Button onClick={() => window.location.reload()}>Refresh</Button>
+        <Button onClick={() => window.location.assign(window.location.origin)}>Go to home</Button>
+      </div>
     </div>
   );
 };
