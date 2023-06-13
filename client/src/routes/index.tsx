ea1a6b5b35add8a1c4afd19b83fe34753055a9c4
joe-shajan <joeshajan1551@gmail.com>
2023-06-13 22:18:51 +0530

    remove unused route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,21 +5,10 @@ import { useAuth } from '@/hooks';
 
 import { protectedRoutes } from './protected';
 
-const Pricing = () => {
-  return (
-    <div className='flex flex-col items-center gap-4 pt-8'>
-      <h1 className='text-4xl'>Pricing page</h1>
-    </div>
-  );
-};
-
 export const AppRoutes = () => {
   const { user } = useAuth();
 
-  const commonRoutes = [
-    { path: '/', element: <Landing /> },
-    { path: '/pricing', element: <Pricing /> },
-  ];
+  const commonRoutes = [{ path: '/', element: <Landing /> }];
 
   const routes = user ? protectedRoutes : [];
 
