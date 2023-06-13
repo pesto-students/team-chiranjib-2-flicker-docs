7667b5d2ee9b356762546cc4726963a7cd78a30b
joe-shajan <joeshajan1551@gmail.com>
2023-06-13 22:19:10 +0530

    redirect if not loggedin

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -3,7 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Suspense, useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { Button, Spinner } from '@/components/';
 import { loadUser } from '@/lib';
@@ -31,11 +31,19 @@ type AppProviderProps = {
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   const [user, setUser] = useState<User>(null);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     (async () => {
       const userData = await loadUser();
-      setUser(userData);
+      if (!userData) {
+        if (location.pathname !== '/') {
+          navigate(`/?redirect=${location.pathname}${location.search}`);
+        }
+      } else {
+        setUser(userData);
+      }
     })();
   }, []);
 
@@ -53,7 +61,9 @@ export const AppProvider = ({ children }: AppProviderProps) => {
             {process.env.NODE_ENV !== 'test' && <ReactQueryDevtools initialIsOpen={false} />}
 
             <AuthContext.Provider value={{ user, setUser }}>
-              <Router>{children}</Router>
+              {/* <Router> */}
+              {children}
+              {/* </Router> */}
             </AuthContext.Provider>
           </QueryClientProvider>
         </HelmetProvider>
